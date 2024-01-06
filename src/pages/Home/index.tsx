a4132fe10b1c8e0dce0e92a9e829de26c7324232
Nathan Lins <nathan.lins012@gmail.com>
2024-01-05 21:27:42 -0300

    add use query pokemon page and card pokemon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,20 @@
-import axios from "axios";
+import { PokemonCard } from "../../components/PokemonCard";
+import { useQuerypokemonPage } from "../../hooks/useQueryPokePage";
 import { Container } from "./style";
 
 export function Home() {
-  const data = axios.get("pokemon")
+  const { data } = useQuerypokemonPage();
   console.log(data);
-  
+
   return (
     <Container>
-      <h1>Home</h1>
+      <h1>"bem vindo à Pokedex do Reprograma Jucás."</h1>
+
+      <div className="gridCards">
+        {data?.map((pokemon) => {
+          return <PokemonCard pokemon={pokemon}/>;
+        })}
+      </div>
     </Container>
   );
 }

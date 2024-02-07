7175315ec125637b52659dded7ce1a7f09fa5f5a
NathanLins12 <nathan.lins012@gmail.com>
2024-02-07 20:03:48 -0300

    fixed pagination home

--- src/hooks/useQueryPokemonPage.ts
+++ src/hooks/useQueryPokemonPage.ts
@@ -7,20 +7,22 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 export function useQueryPokemonPage() {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(30);
-  const [totalPages, setTotalPages] = useState(1);
-  
-  const navigate = useNavigate()
-  const searchParams = useSearchParams()
+  const [totalPages, setTotalPages] = useState(0);
+
+  const navigate = useNavigate();
+  const searchParams = useSearchParams();
 
   async function getPokemonPage({ page = 1, limit = 30 }) {
     const offset = (page - 1) * limit;
     const { data } = await API.get(`/pokemon?limit=${limit}&offset=${offset}`);
 
-    const pokemonPromises = data.results.map(async (pokemon: { url: string }) => {
-      const response = await fetch(pokemon.url);
-      const data = await response.json();
-      return data;
-    });
+    const pokemonPromises = data.results.map(
+      async (pokemon: { url: string }) => {
+        const response = await fetch(pokemon.url);
+        const data = await response.json();
+        return data;
+      }
+    );
 
     const pokemonData = await Promise.all(pokemonPromises);
 
@@ -44,10 +46,24 @@ export function useQueryPokemonPage() {
     navigate(`?page=${page - 1}`);
   }
 
-useEffect(() => {
-  const pageQuery = Number(searchParams[0].get("page"))
-  setPage(pageQuery || 1)
-}, []);
+  useEffect(() => {
+    const pageQuery = Number(searchParams[0].get("page"));
+    setPage(pageQuery || 1);
+
+    if (totalPages > 0) {
+      if (pageQuery > totalPages) {
+        navigate(`?page=${totalPages}`);
+        setPage(totalPages);
+        return;
+      }
+
+      if (pageQuery < 1) {
+        navigate(`?page=1`);
+        setPage(1);
+        return;
+      }
+    }
+  }, [searchParams, page, totalPages, navigate]);
 
   const query = useQuery({
     queryKey: ["getPokemonPage", page, limit],
